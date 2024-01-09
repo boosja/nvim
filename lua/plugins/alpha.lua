dae63301045e26e789e7505573d06fee8c7d070d
Mathias Iversen <mathias.iversen@mattilsynet.no>
2024-01-09 16:01:15 +0100

    dashboard art

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -4,35 +4,33 @@ return {
   --   local dashboard = require "alpha.themes.dashboard"
   --   require('alpha').setup(dashboard.config)
   -- end
-  opts = function()
+  config = function()
+    local alpha = require "alpha"
     local dashboard = require "alpha.themes.dashboard"
-    require "alpha.term"
 
-    dashboard.section.terminal.command = vim.fn.stdpath("config") .. "/lua/nvim-logo -t"
-    dashboard.section.terminal.width = 70
-    dashboard.section.terminal.height = 10
-    dashboard.section.terminal.opts.redraw = true
-    dashboard.section.terminal.opts.window_config.zindex = 1
-
-    dashboard.opts.layout = {
-      dashboard.section.terminal,
-      { type = "padding", val = 4 },
+    dashboard.section.header.val = {
+      [[                         ]],
+      [[  ┏┓┓   ┳┓┏┓┏┓┏┓┏┳┏┓╹┏┓  ]],
+      [[  ┣ ┃   ┣┫┃┃┃┃┗┓ ┃┣┫ ┗┓  ]],
+      [[  ┗┛┗┛  ┻┛┗┛┗┛┗┛┗┛┛┗ ┗┛  ]],
+      [[                                                     ]],
+      [[  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+      [[  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+      [[  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+      [[  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+      [[  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+      [[  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+      [[                                                     ]],
     }
 
-    return dashboard
-  end,
-  config = function(_, dashboard)
-    if vim.o.filetype == 'lazy' then
-      vim.cmd.close()
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'AlphaReady',
-        callback = function()
-          require('lazy').show()
-        end,
-      })
-    end
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "    New file" , ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "    Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+      dashboard.button("r", "    Recent"   , ":Telescope oldfiles<CR>"),
+      dashboard.button("s", "    Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button("q", "    Quit NVIM", ":qa<CR>"),
+    }
 
-    local alpha = require "alpha"
     alpha.setup(dashboard.opts)
   end
 }
