6b09d81537f52d486159d5a018f52932b4392ad6
Mathias Iversen <mathias.iversen@mattilsynet.no>
2024-01-20 20:40:01 +0100

    Added ClojureScript support with conjure

---
+++ lua/plugins/clojure.lua
@@ -0,0 +1,31 @@
+return {
+	"Olical/conjure",
+	ft = { "clojure", "edn" },
+	dependencies = {
+		{
+			"PaterJason/cmp-conjure",
+			config = function()
+				local cmp = require("cmp")
+				local config = cmp.get_config()
+
+				table.insert(config.sources, {
+					name = "buffer",
+					option = {
+						sources = {
+							{ name = "conjure" },
+						},
+					},
+				})
+
+				cmp.setup(config)
+			end,
+		},
+	},
+	config = function()
+		require("conjure.main").main()
+		require("conjure.mapping")["on-filetype"]()
+	end,
+	init = function()
+		vim.g["conjure#debug"] = true
+	end,
+}
