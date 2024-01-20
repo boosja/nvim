ab86147be087663538eb6c72434ec226fd731446
Mathias Iversen <mathias.iversen@mattilsynet.no>
2024-01-20 20:15:31 +0100

    alpha shortcuts

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -5,8 +5,8 @@ return {
   --   require('alpha').setup(dashboard.config)
   -- end
   config = function()
-    local alpha = require "alpha"
-    local dashboard = require "alpha.themes.dashboard"
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
 
     dashboard.section.header.val = {
       [[                         ]],
@@ -24,13 +24,14 @@ return {
     }
 
     dashboard.section.buttons.val = {
-      dashboard.button("e", "    New file" , ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "    Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
-      dashboard.button("r", "    Recent"   , ":Telescope oldfiles<CR>"),
-      dashboard.button("s", "    Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-      dashboard.button("q", "    Quit NVIM", ":qa<CR>"),
+      dashboard.button("e", "  : New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "  : Find git file", ":Telescope git_files<CR>"),
+      dashboard.button("l", "  : Find in folder", ":Telescope find_files<CR>"),
+      dashboard.button("r", "  : Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("s", "  : Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button("q", "  : Quit NVIM", ":qa<CR>"),
     }
 
     alpha.setup(dashboard.opts)
-  end
+  end,
 }
